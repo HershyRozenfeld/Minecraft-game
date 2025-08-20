f1171b4eb4831c988e4cfc50fd2cef58842c3da8
Aaron <aaronmadar2655@gmail.com>
2025-08-20 15:39:27 +0300

    architectura=html-css-js,areah-avanim-bedrock+2button-onthegame

--- js/htmlgenerator.js
+++ js/htmlgenerator.js
@@ -10,15 +10,20 @@ function generateHtml() {
       cell.classList.add("cell");
 
       if (r <= 10) {
-        cell.classList.add("shamaim");
+        cell.classList.add("sky");
       } else if (r === 11) {
         cell.classList.add("deshe");
-      } else {
+      } else if (r > 11 && r < 16 ){
         cell.classList.add("adama");
+      }else if (r >= 16 && r <=28 ){
+        cell.classList.add("avanim")
+      }else {
+        cell.classList.add('bedrock')
       }
+      
 
       body.appendChild(cell);
-    }
+    } 
   }
 }
 
