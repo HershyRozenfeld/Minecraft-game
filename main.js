396af33174e17f5ff23e93dba470f1f137c2a1af
haimassraf <166395151+haimassraf@users.noreply.github.com>
2025-08-20 14:49:09 +0300

    Haim (#21)
    * first commit
    
    * readme001
    
    * fd
    
    * tiles
    
    * creation Doc front- Doc Pictures  - creation file htmlgenerator with function generatehtml
    
    * basic tiles
    
    * file htmlgenerator + function call
    
    * tools
    
    ---------
    
    Co-authored-by: Hershy Rozenfeld <153819469+HershyRozenfeld@users.noreply.github.com>
    Co-authored-by: Aaron <aaronmadar2655@gmail.com>

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
-const body = document.querySelector("body");
+const main = document.querySelector("main");
+const header = document.querySelector('header');
+const shovel = document.getElementsByClassName("shovel")[0];
+const pickaxe = document.getElementsByClassName("pickaxe")[0];
+
+let currentTool = "";
 let currentTile = "";
+
+const tools = {
+    land: "shovel",
+    grass: "shovel",
+    rock: "pickaxe"
+}
+
 const packege = {
+    grass: 0,
     land: 0,
     rock: 0
 };
+
+shovel.addEventListener('click', () => {
+    currentTool = "shovel";
+})
+
+pickaxe.addEventListener('click', () => {
+    currentTool = "pickaxe";
+})
+
 
 function createSky() {
     const sky = document.createElement("div");
@@ -11,48 +33,64 @@ function createSky() {
     sky.addEventListener('click', () => {
         if (currentTile === "land" && packege.land > 0) {
             sky.replaceWith(createTile('land'));
-            packege.land--;
+            removeTile("land");
         }
         else if (currentTile === "rock" && packege.rock > 0) {
             sky.replaceWith(createTile('rock'));
-            packege.rock--;
+            removeTile("rock");
+        }
+        else if (currentTile === "grass" && packege.grass > 0) {
+            sky.replaceWith(createTile('grass'));
+            removeTile("grass");
         }
     })
     return sky;
 }
 
-function createTile(type) {
-    const land = document.createElement("div");
-    land.classList = type;
-    land.addEventListener('click', () => {
-        land.replaceWith(createSky());
-        packege[type]++;
-        currentTile = type;
-        console.log(packege);
+function removeTile(type) {
+    packege[type]--;
+    const currentPackage = document.querySelector(`.package > .${type}`);
+    currentPackage.textContent = packege[type];
+    if (packege[type] === 0) {
+        currentPackage.style.display = "none";
+    }
+}
 
+function createTile(type) {
+    const tile = document.createElement("div");
+    tile.classList = type;
+    tile.addEventListener('click', () => {
+        if (currentTool === tools[type]) {
+            tile.replaceWith(createSky());
+            packege[type]++;
+            const currentPackage = document.querySelector(`.package > .${type}`);
+            currentPackage.style.display = "inline";
+            currentPackage.textContent = packege[type];
+            currentTile = type;
+        }
     })
-    return land;
+    return tile;
 }
 
 function init() {
     for (let i = 0; i < 500; i++) {
         const sky = createSky();
-        body.appendChild(sky);
+        main.appendChild(sky);
     }
 
     for (let i = 0; i < 50; i++) {
         const grass = createTile("grass");
-        body.appendChild(grass);
+        main.appendChild(grass);
     }
 
     for (let i = 0; i < 250; i++) {
         const land = createTile('land');
-        body.appendChild(land)
+        main.appendChild(land)
     }
 
     for (let i = 0; i < 1000; i++) {
         const rock = createTile('rock');
-        body.appendChild(rock);
+        main.appendChild(rock);
     }
 }
 
