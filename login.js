21b0a1c83f4bcb248daf3233415fd92addc1c790
Hershy Rozenfeld <153819469+HershyRozenfeld@users.noreply.github.com>
2025-08-20 12:34:37 +0300

    Login page (#17)
    * add Minecraft video file
    
    * add login.css for styling the login page
    
    * add login.html for the login page with background video
    
    * refactor login.css for improved layout and readability
    
    * refactor login.html for improved structure and readability
    
    * add login.js for fullscreen functionality and event handling
    
    * refactor header styles for improved clarity
    
    * refactor login.html for improved structure and RTL support
    
    * remove unnecessary alert and console log from fullscreen functionality

--- login.js
+++ login.js
@@ -1,9 +1,6 @@
-alert("aaa")
 const fsBtn = document.getElementById('fullscreen-btn');
 
 fsBtn.addEventListener('click', () => {
-    console.log('hi');
-
     if (!document.fullscreenElement) {
         document.documentElement.requestFullscreen().catch(err => {
             alert(`שגיאה במסך מלא: ${err.message}`);
